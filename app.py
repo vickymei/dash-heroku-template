b568d3a7da078fbc7313440ba8db8ef2fadb30ab
Mei <vickymei713@gmail.com>
2019-09-11 18:52:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+###########  your variables
+beers=['test test', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
 ibu_values=[35, 60, 85, 75]
 abv_values=[5.4, 7.1, 9.2, 4.3]
 color1='lightblue'
